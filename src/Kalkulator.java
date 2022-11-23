533d0b18f83dfc5380a495da81d9632e132629c8
KacperGil <kacpergil1610@gmail.com>
2022-11-23 20:55:44 +0100

    kalkulator ze switcha2

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -4,9 +4,9 @@ public class Kalkulator {
     public static void main(String[] args) {
         System.out.println("Witaj! Podaj pierwszą cyfrę");
         Scanner scanner=new Scanner(System.in);
-        int a=scanner.nextInt();
+        double a=scanner.nextDouble();
         System.out.println("Podaj drugą cyfrę");
-        int b=scanner.nextInt();
+        double b=scanner.nextDouble();
         System.out.println("Wybierz akcje \ndodawanie(1) \nodejmowanie(2) \nmnożenie(3) \ndzielenie(4)");
         int akcja=scanner.nextInt();
         switch (akcja){
@@ -17,11 +17,20 @@ public class Kalkulator {
                 System.out.println(a-b);
                 break;
             case 3:
+
                 System.out.println(a*b);
                 break;
             case 4:
-                System.out.println(a/b);
-                break;
+                if(b==0){
+                    System.out.println("Cholero nie dziel przez zero!");
+                    break;
+                }else {
+                    System.out.println(a/b);
+                    break;
+                }
+            default:
+                System.out.println("Nie ma takiej metody!");
+
 
         }
     }
